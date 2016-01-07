7b046932cb22301235bf053e42d781eb6cf11693
Alan8090 <Alan8090@163.com>
2016-01-07 10:23:55 +0800

    checkout to github

--- SanyJBZ/app/src/main/java/com/sanyjbz/alan8090/sanyjbz/MainActivity.java
+++ SanyJBZ/app/src/main/java/com/sanyjbz/alan8090/sanyjbz/MainActivity.java
@@ -1,6 +1,9 @@
 package com.sanyjbz.alan8090.sanyjbz;
 
+
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,7 +20,7 @@ import android.widget.Toast;
 
 import com.sanyjbz.alan8090.sanyjbz.Alan_Control.midToast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +28,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     */
+private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = MainActivity.this;
+
         FrameLayout frame = (FrameLayout) findViewById(R.id.mylayout);
         final MeziView mezi = new MeziView(MainActivity.this);
         //为我们的萌妹子添加触摸事件监听器
@@ -57,10 +64,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+       Button btn_alertdialog=(Button)findViewById(R.id.btn_alterdialog);
+        btn_alertdialog.setOnClickListener(this);
     }
+    private AlertDialog alert = null;
+    private AlertDialog.Builder builder = null;
+
+    @Override
+   public void onClick(View v)
+   {
+      switch (v.getId()){
+          //普通对话框
+          case R.id.btn_alterdialog:
+
+          alert=null;
+              builder = new AlertDialog.Builder(mContext);
+              alert=builder.setIcon(R.drawable.s_1)
+                  .setTitle("系统提示")
+                  .setMessage("这是一个最普通的AlertDialog,\\n带有三个按钮，分别是取消，中立和确定")
+                  .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                              @Override
+                              public void onClick(DialogInterface dialog, int which) {
+                                  Toast.makeText(mContext, "你点击了取消按钮~", Toast.LENGTH_SHORT).show();
+
+                              }
+                          }
+                  )
+                          .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                              @Override
+                              public void onClick(DialogInterface dialog, int which) {
+                                  Toast.makeText(mContext, "你点击了确定按钮~", Toast.LENGTH_SHORT).show();
+                              }
+                          })
+                              .setNeutralButton("中立", new DialogInterface.OnClickListener() {
+                                  @Override
+                                  public void onClick(DialogInterface dialog, int which) {
+                                      Toast.makeText(mContext, "你点击了中立按钮~", Toast.LENGTH_SHORT).show();
+                                  }
+                              }).create();             //创建AlertDialog对象
+                      alert.show();                    //显示对话框
+                      break;
 
 
+                  }
+   }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
